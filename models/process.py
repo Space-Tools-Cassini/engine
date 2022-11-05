c63fe6631cfd296c2a0da22ee2c5403510e77f84
MaFe1401 <marcferre42@gmail.com>
2022-11-05 21:58:09 +0100

    update

---
+++ models/process.py
@@ -0,0 +1,8 @@
+
+class Process:
+    scored_crops=[]
+    skipped_crops=[]
+    
+    def __init__(self,lat,lon):
+        self.lat=lat
+        self.lon=lon
--- recommender/engine.py
+++ recommender/engine.py
@@ -4,6 +4,31 @@
 # This is the main code of the recommendator engine
 # This must be a simple API.
 
+class Process:
+    
+    def __init__(self,lat,lon,time_init):
+        self.lat=lat #latitude
+        self.lon=lon #longitude 
+        self.scored_crops=[] #scored crops
+        self.skipped_crops=[] #discarded crops
+        self.time_init=time_init #Time where process starts
+        self.time_stop = 0 #Time where process is ended
+
+    def add_scored_crops(self, scored_crop):
+        self.scored_crops.append(scored_crop)
+    
+    def add_skipped_crops(self, skipped_crop):
+        self.skipped_crops.append(skipped_crop)
+
+class Mapper:
+
+    def __init__(self):
+        self.crop_score = []
+
+    def add_crop_score(self, crop, score):
+        self.crop_score.append((crop,score))
+        
+
 def analize():
     """
     def 
