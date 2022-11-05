3471f0b06578ffe0d1727b42a193d13811b03489
as43z <albert.saez.nunez@estudiantat.upc.edu>
2022-11-05 18:32:58 +0100

    start the engine

---
+++ recommender/__init__.py
@@ -0,0 +1,6 @@
+# ENGINE
+#
+# This is the recommendator engine, score based.
+
+def get_version():
+    return "alpha-0.0.1"
---
+++ recommender/engine.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#
+# Engine module
+# This is the main code of the recommendator engine
+# This must be a simple API.
+
+def analize():
+    # TODO:
+    # - Implement the Process initializer.
+    # - Implement the DataManager.
+    # - Implement the Mapper.
+    #
+    # Process Inits --> DataManager gets and process data 
+    # --> Mapper --> Add results of the mapping to the Process
+    # --> return the Process data.
+    pass
